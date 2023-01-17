47b6a6d0d041cf86bb1ad8b799424248dd270611
Abdurahim Hakimov <abdurahimxakimov2@gmail.com>
2023-01-17 22:30:00 +0500

    updated by Abdurahim

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
+from django.core.mail import send_mail
 # Project
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 class PostListView(ListView):
@@ -21,18 +23,44 @@ def post_detail(request, year, month, day, post):
     #     raise Http404('No Post found')
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
                              slug=post, publish__year=year, publish__month=month, publish__day=day)
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
-    return render(request, 'post/detail.html', {'post': post})
+    return render(request, 'post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
 
         if form.is_valid():
             cd = form.cleaned_data
 
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+
+            subject = f"{cd['name']} recommends you read {post.title}"
+
+            message = f"Read {post.title} at {post_url}\n\n {cd['name']}\'s comments: {cd['comments']}"
+
+            send_mail(subject, message, 'supercellid.4910@gmail.com', ['supercellid.4910@gmail.com'])
+            sent = True
+
     else:
         form = EmailPostForm()
-    return render(request, 'post/share.html', {'post': post, 'form': form})
+    return render(request, 'post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, './includes/comment.html', {'post': post, 'form': form, 'comment': comment})
